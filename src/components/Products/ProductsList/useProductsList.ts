2c472e0906a93959feb9d2814fbdcdbe5581be01
lukaasz555 <97296160+lukaasz555@users.noreply.github.com>
2024-01-25 18:40:38 +0100

    init store + productsReducer

--- src/components/Products/ProductsList/useProductsList.ts
+++ src/components/Products/ProductsList/useProductsList.ts
@@ -1,26 +1,30 @@
 import { useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import { IProduct } from '../../../interfaces/IProduct';
+import { useEffect } from 'react';
 import { ProductsService } from '../../../services/FakeStore/Products/ProductsService';
+import { store } from '../../../store/store';
+import { setProducts } from '../../../store/productsSlice/productsReducer';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../store/store';
 
 export const useProductsList = () => {
-	const [productsList, setProductsList] = useState<IProduct[]>([]);
 	const { category } = useParams();
+	const products = useSelector((state: RootState) => state.products.products);
+
+	const fetchProducts = async () => {
+		try {
+			const productService = new ProductsService();
+			const res = category
+				? await productService.getProductsByCategory(category)
+				: await productService.getProducts();
+			store.dispatch(setProducts(res));
+		} catch (err) {
+			console.log('fetchProducts err ', err);
+		}
+	};
 
 	useEffect(() => {
-		const fetchProducts = async () => {
-			try {
-				const productService = new ProductsService();
-				const res = category
-					? await productService.getProductsByCategory(category)
-					: await productService.getProducts();
-				setProductsList(res);
-			} catch (err) {
-				console.log('fetchProducts err ', err);
-			}
-		};
 		fetchProducts();
 	}, []);
 
-	return { productsList };
+	return { productsList: products };
 };
